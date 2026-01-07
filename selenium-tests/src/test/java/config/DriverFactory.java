c29b4f80f481b03afa2a077264276c92892ce7b3
AntoineLudwigGalli <antoineludwiggalli@gmail.com>
2026-01-07 17:05:10 +0100

    modif selenium pour régler webdriver

--- selenium-tests/src/test/java/config/DriverFactory.java
+++ selenium-tests/src/test/java/config/DriverFactory.java
@@ -3,6 +3,9 @@ package config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
 
 public class DriverFactory {
 
@@ -12,8 +15,8 @@ public class DriverFactory {
 
 
     public static WebDriver createDriver() {
-        ChromeOptions options = new ChromeOptions();
 
+        ChromeOptions options = new ChromeOptions();
         options.addArguments(
                 "--headless=new",
                 "--no-sandbox",
@@ -22,6 +25,13 @@ public class DriverFactory {
                 "--window-size=1920,1080"
         );
 
-        return new ChromeDriver(options);
+        try {
+            return new RemoteWebDriver(
+                    new URL("http://localhost:4444/wd/hub"),
+                    options
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Impossible de créer le driver Selenium", e);
+        }
     }
 }
